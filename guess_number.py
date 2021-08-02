105f0e86655ecb4aaba3c9d9907ce1177a63d678
rdm <rdmunden@rocketmail.com>
2021-08-02 18:03:04 -0500

    minor iteration

--- guess_number.py
+++ guess_number.py
@@ -8,10 +8,10 @@ def try_digit(n):
     # if n >= 63107:
     # if n >= 9759384755:
     # if n >= 2112:
-    # if n >= 3178:
+    if n >= 3178:
     # if n >= 3199:
     # if n >= 69105:
-    if n >= 999:
+    # if n >= 999:
         print (f'was true: {n}')
         return n
 
@@ -21,25 +21,32 @@ def get_num_digits(n=9):
     """ This is going to assume the number you want is at least 1
         So it's positive non-zero
     """
-    ''' this actually returns the largest number with the correct number of digits '''
     # if is_over_limit(n):
     if try_digit(n):
-        return n
+        # return n
+        return len(str(n))
     else:
         return get_num_digits((n * 10) + 9)
 
 def guess_number(N):
-    c = len(str(N))
+    # c = len(str(N))
+    c = N
     d = 0
+    # N = int("9" * c)
 
     for f in range (1,c+1):
         print ('----')
         print (f'{d=}')
         for i in range(10*d, 10*d+9+1):
             print (f'{i=}')
-            print ('trying {}'.format(i*10**(c-f) + (N//(10**f))))
+            # print ('trying {}'.format(i*10**(c-f) + (N//(10**f))))
+            #print ('trying {}'.format(i*10**(c-f) + (int("9" * (c-f)))))
 
-            if next_digit := try_digit(i*10**(c-f) + (N//(10**f))):
+            # if next_digit := try_digit(i*10**(c-f) + (N//(10**f))):
+            # xx = ("9" * (c-f)) or "0"
+            # if next_digit := try_digit(i*10**(c-f) + (int(("9" * (c-f)) or "0"))):
+            # if next_digit := try_digit(i * 10**(c-f) + 10**(c-f) - 1):
+            if next_digit := try_digit((i+1) * 10**(c-f) - 1):
                 d = next_digit // 10**(c-f)
                 break
 
