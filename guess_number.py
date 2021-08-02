e2765d822c9df55b9962f345104232246336338f
rdm <rdmunden@rocketmail.com>
2021-08-02 18:46:39 -0500

    simplifications and added timing info

--- guess_number.py
+++ guess_number.py
@@ -3,21 +3,25 @@
     So it's a positive, non-zero
 """
 
+
 def try_digit(n):
     # looking_for = 7897
     # looking_for = 63107
     # looking_for = 9759384758
     # looking_for = 2112
-    looking_for = 3178
+    # looking_for = 3178
     # looking_for = 3199
     # looking_for = 69105
     # looking_for = 999
     # looking_for = 1
     # looking_for = 0
     # looking_for = -123
+    # looking_for = 179769313486231580793728971405303415079934132710037826936173778980444968292764750946649017977587207096330286416692887910946555547851940402630657488671505820681908902000708383676273854845817711531764475730270069855571366959622842914819860834936475292719074168444365510704342711559699508093042880177904174497791
+    # looking_for = 487928273
+    looking_for = 500000000
 
     if n >= looking_for:
-        print (f'The value {n} was greater than or equal to the target.')
+        # print (f'The value {n} was greater than or equal to the target.')
         return n
 
 
@@ -31,20 +35,19 @@ def get_num_digits(n=9):
 def guess_number(N):
     msd = 0                     # The digits found so far
 
-    digits_remaining = N-1      # The # digits to the right of the one we're working on
+    digits_remaining = N-1      # The # digits remaining to the right of the one we're working on
 
     while digits_remaining >= 0:
-        print ('----')
-        if msd:
-            # msg = format(msd, f'.<{N}')
-            msg = format(msd, f'░<{N}')
-        else:
-            msg = "None"
-        print (f'Digits uncovered so far: {msg}')
+        # print ('----')
+        # if msd:
+        #     msg = format(msd, f'░<{N}')
+        # else:
+        #     msg = "None"
+        # print (f'Digits found: {msg}')
 
         for d in range(10*msd, 10*msd+10):
             ''' msd represents the most significant digits found so far
-                10*msd shifts them to the left so we can find the next significant digit after them
+                "10*msd" shifts them to the left so we can find the next significant digit after them
                 e.g. if two msd's have been found, and they are 19, this will loop over the range:
                 190
                 191
@@ -53,7 +56,7 @@ def guess_number(N):
                 199 
             '''
             
-            print (f'\tTrying significant digits: {d=}', end='')
+            # print (f'\tTrying significant digits: {d=}', end='')
 
             ''' Even though it is a mathematical simplification to express this formula as:
                 (d+1) * 10**digits_remaining - 1
@@ -79,8 +82,7 @@ def guess_number(N):
                     19999
             '''
             try_num = (d * 10**digits_remaining) + (10**digits_remaining - 1)
-            print ('\t... trying: {:0{width}}'.format(try_num, width=N))
-
+            # print ('\t... trying: {:0{width}}'.format(try_num, width=N))
 
             if next_digit := try_digit(try_num):
                 msd = next_digit // 10**digits_remaining
@@ -91,8 +93,42 @@ def guess_number(N):
     return msd
 
 
-N = get_num_digits()
-print(f'Number of digits: {N}')
-# print (d)
-ans = guess_number(N)
-print(f'Answer is: {ans}')
+def find_hard_way():
+    i = 1
+    while 1:
+        if result := try_digit(i):
+            return result
+        i += 1
+
+
+def find_hard_way2(n):
+    i = 10**(n-1)
+    while 1:
+        if result := try_digit(i):
+            return result
+        i += 1
+
+
+def main():
+    import time
+    t = time.time()
+    N = get_num_digits()
+    print(f'Number of digits: {N}')
+    # print (d)
+    ans = guess_number(N)
+    print(f'Answer is: {ans}')
+    print("Elapsed time: {}".format(time.time()-t))     # to find the biggest number (309 digits) was under 7 ms
+
+    # t = time.time()
+    # res = find_hard_way()
+    # print ('found hard way: {}'.format(res))
+    # print("Elapsed time: {}".format(time.time()-t))     # This took ~1:41 to find the 9 digit number 500,000,000
+    #
+    # t = time.time()
+    # res = find_hard_way2(N)
+    # print ('found hard way 2: {}'.format(res))
+    # print("Elapsed time: {}".format(time.time()-t))     # This took ~1:27 to find the 9 digit number 500,000,000
+
+
+if __name__ == '__main__':
+    main()
