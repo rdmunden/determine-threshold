42b727c4bf9b7287c67d6f9cde4dfbc596e1d41c
rdm <rdmunden@rocketmail.com>
2021-08-02 18:33:22 -0500

    changed var i to d, and introduced var msd == d+1

--- guess_number.py
+++ guess_number.py
@@ -8,11 +8,13 @@ def try_digit(n):
     # if n >= 63107:
     # if n >= 9759384755:
     # if n >= 2112:
-    if n >= 3178:
+    # if n >= 3178:
     # if n >= 3199:
-    # if n >= 69105:
+    if n >= 69105:
     # if n >= 999:
-        print (f'was true: {n}')
+    #     print (f'was true: {n}')
+    #     print (f'Was greater than or equal to the value: {n}')
+        print (f'The value {n} was greater than or equal to the target.')
         return n
 
 #----------
@@ -31,35 +33,74 @@ def get_num_digits(n=9):
 def guess_number(N):
     # c = len(str(N))
     # c = N
-    d = 0
+    msd = 0
     # N = int("9" * c)
 
     # for f in range (1,N+1):
     for f in range (N-1,-1,-1):
         print ('----')
-        print (f'Most significant digits found: {d=}')
-        for i in range(10*d+1, 10*d+9+2):
-            ''' Even though it is mathematically correct and a simplification to use "10*d+1" to "10*d+11" here
-                and use just use "i" down below,
-                I think it makes the code easier to visualize by changing the range to "10*d" to "10*d+10" and
-                using "i+1" below
+        # print (f'Most significant digits found so far: {msd=}')
+        # print (f'Most significant digits found so far: {msd or "None"}')
+        if msd:
+            msg = format(msd, f'.<{N}')
+        else:
+            msg = "None"
+        print (f'Digits uncovered so far: {msg}')
+
+        for d in range(10*msd, 10*msd+9+1):
+            ''' d represents the most significant digits found so far
+                10*d shifts them to the left so we can find the next significant digit after them
+                e.g. if two msds have been found, and they are 19, this will loop over the range:
+                190
+                191
+                192
+                ...
+                199 
             '''
-            print (f'{i=}')
+            # print (f'\tMost significant digits being tried: {d=}')
+            # print (f'\tTrying significant digits: {d=}', end='')
+
             # print ('trying {}'.format(i*10**(c-f) + (N//(10**f))))
             #print ('trying {}'.format(i*10**(c-f) + (int("9" * (c-f)))))
-            print ('trying {}'.format(i * 10**f - 1))
-            
+            # print ('trying {}'.format(i * 10**f - 1))
+            # print ('\tNumber being tried: {:0{width}}'.format((d * 10**f + 10**f - 1), width=N))
+            print ('\t... trying number'
+                   ': {:0{width}}'.format((d * 10**f + 10**f - 1), width=N))
+
             # if next_digit := try_digit(i*10**(c-f) + (N//(10**f))):
             # xx = ("9" * (c-f)) or "0"
             # if next_digit := try_digit(i*10**(c-f) + (int(("9" * (c-f)) or "0"))):
             # if next_digit := try_digit(i * 10**(c-f) + 10**(c-f) - 1):
             # if next_digit := try_digit(i * 10**(N-f) - 1):
-            if next_digit := try_digit(i * 10**f - 1):
+
+            # if next_digit := try_digit((i+1) * 10**f - 1):
+            ''' Even though it is a mathematical simplification to express this formula as:
+                (i+1) * 10**f - 1
+                I am multiplying out the (i+1) in order to make it more clear how this works
+            '''
+            if next_digit := try_digit(d * 10**f + 10**f - 1):
+                ''' if there are 5 digits in the answer
+                    and we have found the two most significant digits
+                    and say e.g. they are 19 (which is represented by "i")
+                    then "i * 10**f" is looping over:
+                        190 * 100 = 19000
+                        191 * 100 = 19100
+                        192 * 100 = 19200
+                        ...
+                        199 * 100 = 19900
+                    To that we are adding "10**f - 1", which in this case is 100 - 1 = 99
+                    So the actual numbers being tried are:
+                        19099
+                        19199
+                        19299
+                        ...
+                        19999
+                '''
                 # d = next_digit // 10**(N-f)
-                d = next_digit // 10**f
+                msd = next_digit // 10**f
                 break
 
-    return d
+    return msd
 
 
 print ('====')
